411a85a6ece3deffb26e54a82bb1b9dac5887248
madilyn <lollin0@naver.com>
2021-10-03 18:52:11 +0900

    ➕ [ADD]: New project, Set OS target

---
+++ 29th-week1-homework/29th-week1-homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  29th-week1-homework
+//
+//  Created by 1v1 on 2021/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
