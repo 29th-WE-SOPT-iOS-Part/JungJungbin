a80ba848374871ea5365639fef9dd84a8b2b5631
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2021-10-09 17:02:15 +0900

    [CHORE] Create VC

---
+++ week2-seminar/week2-seminar/CustomTabBar/CustomTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabBarController.swift
+//  week2-seminar
+//
+//  Created by 1v1 on 2021/10/09.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
