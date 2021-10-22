27a73a3e0cc2339152136ad49f3769cbd4dac9de
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2021-10-22 11:30:13 +0900

    [ADD] Create VCs

--- 29th-homework/29th-homework/Common/CustomTabBarController.swift
+++ 29th-homework/29th-homework/Common/CustomTabBarController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class CustomTabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setTabBar()
     }
-
+    
+    func setTabBar() {
+        guard let redVC = self.storyboard?.instantiateViewController(withIdentifier: "redVC"),
+              let orangeVC = self.storyboard?.instantiateViewController(withIdentifier: "orangeVC"),
+              let yellowVC = self.storyboard?.instantiateViewController(withIdentifier: "yellowVC"),
+              let greenVC = self.storyboard?.instantiateViewController(withIdentifier: "greenVC"),
+              let blueVC = self.storyboard?.instantiateViewController(withIdentifier: "blueVC")
+        else { return }
+        
+        redVC.tabBarItem.title = "Home"
+        redVC.tabBarItem.image = UIImage(systemName: "house")
+        redVC.tabBarItem.selectedImage = UIImage(systemName: "house.fill")
+        orangeVC.tabBarItem.title = "Profile"
+        orangeVC.tabBarItem.image = UIImage(systemName: "person")
+        orangeVC.tabBarItem.selectedImage = UIImage(systemName: "person.fill")
+        
+        setViewControllers([redVC, orangeVC, yellowVC, greenVC, blueVC], animated: true)
+    }
 }
