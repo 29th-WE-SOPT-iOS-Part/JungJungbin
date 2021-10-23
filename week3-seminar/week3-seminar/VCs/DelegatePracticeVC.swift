cdb0cbfa5ea1c853b3d8c80bfea9f08e56fa1c9f
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2021-10-23 15:48:40 +0900

    Practice DataSource

--- week3-seminar/week3-seminar/VCs/DelegatePracticeVC.swift
+++ week3-seminar/week3-seminar/VCs/DelegatePracticeVC.swift
@@ -12,9 +12,28 @@ class DelegatePracticeVC: UIViewController {
     @IBOutlet weak var partImageView: UIImageView!
     @IBOutlet weak var partPickerView: UIPickerView!
     @IBOutlet weak var textField: UITextField!
+    
+    var partList: [PartData] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         textField.delegate = self
+        partPickerView.dataSource = self
+        partPickerView.delegate = self
+        
+        initPartData()
+        partImageView.image = partList[0].makeImage()
+        textField.text = partList[0].partName
+    }
+    
+    func initPartData() {
+        partList.append(contentsOf: [
+            PartData(imageName: "wesoptiOSPart", partName: "iOS"),
+            PartData(imageName: "wesoptAndroidPart", partName: "Android"),
+            PartData(imageName: "wesoptWebPart", partName: "Web"),
+            PartData(imageName: "wesoptServerPart", partName: "Server"),
+            PartData(imageName: "wesoptDesignPart", partName: "Design"),
+            PartData(imageName: "wesoptPlanPart", partName: "Plan")
+        ])
     }
 }
 
@@ -39,5 +58,27 @@ extension DelegatePracticeVC: UITextFieldDelegate {
 #endif
         textField.endEditing(true)
         return true
+    }
+}
+
+extension DelegatePracticeVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return partList.count
+    }
+    
+    
+}
+
+extension DelegatePracticeVC: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return partList[row].partName
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        partImageView.image = partList[row].makeImage()
+        textField.text = partList[row].partName
     }
 }
