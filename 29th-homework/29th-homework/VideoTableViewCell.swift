77633341bbc3e41953300e3ed3b76da0caa931b7
Jungbin Jung <lollin0@naver.com>
2021-10-27 10:47:14 +0900

    [CHORE] UI 배치
    Add Color Extension
    Add TableViewCell

---
+++ 29th-homework/29th-homework/VideoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  VideoTableViewCell.swift
+//  29th-homework
+//
+//  Created by 1v1 on 2021/10/27.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
