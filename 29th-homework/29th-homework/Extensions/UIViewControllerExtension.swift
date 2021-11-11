e5e352e534208e226ea34178e337f4cb10081c38
Jungbin Jung <lollin0@naver.com>
2021-11-11 23:05:49 +0900

    Update UIViewControllerExtension.swift

--- 29th-homework/29th-homework/Extensions/UIViewControllerExtension.swift
+++ 29th-homework/29th-homework/Extensions/UIViewControllerExtension.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 extension UIViewController {
-    func alert(_ message: String, completion: (()->Void)? = nil) {
+    func alert(title: String?, message: String, completion: (()->Void)? = nil) {
         DispatchQueue.main.async {
-            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let alert = UIAlertController(title: title ?? nil, message: message, preferredStyle: .alert)
             let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
                 completion?()
             }
