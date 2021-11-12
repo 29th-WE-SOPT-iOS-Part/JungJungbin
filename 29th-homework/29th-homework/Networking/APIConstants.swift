926f4296169aa4be87616aa607fcaa03fbad7c7c
Jungbin Jung <lollin0@naver.com>
2021-11-12 10:32:38 +0900

    [ADD] sign up url

--- 29th-homework/29th-homework/Networking/APIConstants.swift
+++ 29th-homework/29th-homework/Networking/APIConstants.swift
@@ -10,6 +10,6 @@ struct APIConstants {
     static let baseURL = "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api"
     
     // MARK: Feature URL
-    static let loginURL = baseURL + "/user/login"
-    static let readUserURL = baseURL + "/user/"
+    static let signInURL = baseURL + "/user/login"
+    static let signUpURL = baseURL + "/user/signup"
 }
