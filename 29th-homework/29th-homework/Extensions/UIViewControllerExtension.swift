0f2430ab3c5d13b3bf081177ab83ed3590140d1d
Jungbin Jung <lollin0@naver.com>
2021-11-12 10:27:58 +0900

    [ADD] Create SignUpModel

--- 29th-homework/29th-homework/Extensions/UIViewControllerExtension.swift
+++ 29th-homework/29th-homework/Extensions/UIViewControllerExtension.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIViewController {
-    func alert(title: String?, message: String, completion: (()->Void)? = nil) {
+    func alert(title: String?, message: String, completion: (() -> Void)? = nil) {
         DispatchQueue.main.async {
             let alert = UIAlertController(title: title ?? nil, message: message, preferredStyle: .alert)
             let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
---
+++ 29th-homework/29th-homework/Views/SignUp/SignUpModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpModel.swift
+//  29th-homework
+//
+//  Created by 1v1 on 2021/11/12.
+//
+
+struct SignUpnResponseData: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: SignInResultData?
+}
+
+struct SignUpResultData: Codable {
+    let id: Int
+    let name, email: String
+}
