eceaebe7f7dfdd319250a33469995342f97bf4cb
Jungbin Jung <lollin0@naver.com>
2021-11-12 11:04:36 +0900

    [ADD] UserDefaults에서 사용할 constant key 정의

---
+++ 29th-homework/29th-homework/Common/UserDefaultsConst.swift
@@ -0,0 +1,11 @@
+//
+//  UserDefaultsConst.swift
+//  29th-homework
+//
+//  Created by 1v1 on 2021/11/12.
+//
+
+class UserDefaultsConst {
+    static let shared = UserDefaultsConst()
+    let userName = "userName"
+}
