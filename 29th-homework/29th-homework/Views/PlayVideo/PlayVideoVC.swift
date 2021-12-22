fef86d0f6311e972e3179d13a2b889795696064b
Jungbin Jung <lollin0@naver.com>
2021-12-23 01:07:56 +0900

    Chore: PlayVideoVC configureUI(), tapDismissBtn() 작성

--- 29th-homework/29th-homework/Views/PlayVideo/PlayVideoVC.swift
+++ 29th-homework/29th-homework/Views/PlayVideo/PlayVideoVC.swift
@@ -10,9 +10,28 @@ import UIKit
 class PlayVideoVC: UIViewController {
     static let ID = "PlayVideoVC"
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    var receivedData = VideoDataForm()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        imageView.image = receivedData.thumbnailImage
+        titleLabel.text = receivedData.title
+        infoLabel.text = "조회수 \(receivedData.viewsM)만회 ・ \(receivedData.uploadWeeks)주 전"
+    }
+    
+    @IBAction func tapDismissBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tapExpandBtn(_ sender: Any) {
+        // Expand ..
     }
 }
