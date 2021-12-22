f5d05c7265149f26c994b3c9cb41f10e1071c95f
Jungbin Jung <lollin0@naver.com>
2021-12-22 23:25:55 +0900

    Create: VideoInfoModel

---
+++ 29th-homework/29th-homework/Views/PlayVideo/Models/VideoInfoModel.swift
@@ -0,0 +1,16 @@
+//
+//  VideoInfoModel.swift
+//  29th-homework
+//
+//  Created by 1v1 on 2021/12/22.
+//
+
+import Foundation
+import UIKit
+
+struct VideoInfoModel {
+    var image: UIImage
+    var title: String
+    var viewCount: Int
+    var date: Date
+}
