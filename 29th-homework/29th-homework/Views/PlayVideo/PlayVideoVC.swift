5d01691b54cbded45bc0233bb4fa0380150b04fe
Jungbin Jung <lollin0@naver.com>
2021-12-22 23:15:27 +0900

    Add: PlayVideoVC, Storyboard 생성

---
+++ 29th-homework/29th-homework/Views/PlayVideo/PlayVideoVC.swift
@@ -0,0 +1,18 @@
+//
+//  PlayVideoVC.swift
+//  29th-homework
+//
+//  Created by 1v1 on 2021/12/22.
+//
+
+import UIKit
+
+class PlayVideoVC: UIViewController {
+    static let ID = "PlayVideoVC"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
