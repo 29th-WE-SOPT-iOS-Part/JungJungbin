ba4b938724869f5062205f3a91ba47b8f983820a
Jungbin Jung <lollin0@naver.com>
2021-12-23 01:06:12 +0900

    Chore: Custom TapGestureRecognizer 생성하여 적용

---
+++ 29th-homework/29th-homework/Views/Home/VideoInfoTapGesture.swift
@@ -0,0 +1,13 @@
+//
+//  VideoInfoTapGesture.swift
+//  29th-homework
+//
+//  Created by 1v1 on 2021/12/23.
+//
+
+import Foundation
+import UIKit
+
+class VideoInfoTapGesture: UITapGestureRecognizer {
+    var videoInfo: VideoDataForm?
+}
